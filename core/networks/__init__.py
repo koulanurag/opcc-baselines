13da53e7d0484722a38b5b2752c9fc5d57b072ae
Anurag Koul <koulanurag@gmail.com>
2022-01-06 17:37:58 -0800

    refactor(network): move commone methods to utils

--- core/networks/__init__.py
+++ core/networks/__init__.py
@@ -1,10 +1,1 @@
-import numpy as np
-import torch
-import torch.nn as nn
-
 from .ensemble import EnsembleDynamicsNetwork
-
-
-
-
-
--- core/networks/ff.py
+++ core/networks/ff.py
@@ -3,23 +3,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 from .base import Base
 import numpy as np
-
-
-def weights_init(m):
-    def truncated_normal_init(t, mean=0.0, std=0.01):
-        torch.nn.init.normal_(t, mean=mean, std=std)
-        while True:
-            cond = torch.logical_or(t < mean - 2 * std, t > mean + 2 * std)
-            if not torch.sum(cond):
-                break
-            t = torch.where(cond, torch.nn.init.normal_(torch.ones(t.shape),
-                                                        mean=mean, std=std), t)
-        return t
-
-    if type(m) == nn.Linear:
-        input_dim = m.in_features
-        truncated_normal_init(m.weight, std=1 / (2 * np.sqrt(input_dim)))
-        m.bias.data.fill_(0.0)
+from .utils import weights_init
 
 
 class FFDynamicsNetwork(Base, nn.Module):
