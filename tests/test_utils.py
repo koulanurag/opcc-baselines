e7ab0a7db52b1e340215307a1423498877dd1289
Anurag Koul <koulanurag@gmail.com>
2022-01-12 12:56:05 -0800

    tests: added test for mc-return

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,44 +1,67 @@
 import gym
-import pytest
+import numpy as np
 import torch
 
 
-# ('Hopper-v2', 'random', 5, 5, 'feed-forward', 3)])
-# @pytest.mark.parametrize('env_name, dataset_name, n_step, num_ensemble, dynamics_type,horizon',
-#                          [('d4rl:maze2d-open-v0', '1k', 5, 3, 'feed-forward', 20)])
-# def test_mc_return(env_name, dataset_name, n_step, num_ensemble, dynamics_type,
-#                    horizon):
-#     import cque
-#     from core.networks import EnsembleDynamicsNetwork
-#     from core.replay_buffer import ReplayBuffer
-#     from core.utils import mc_return
-#     batch_size = 10
-#     env = gym.make(env_name)
-#     network = EnsembleDynamicsNetwork(env_name=env_name,
-#                                       dataset_name=dataset_name,
-#                                       num_ensemble=num_ensemble,
-#                                       obs_size=env.observation_space.shape[0],
-#                                       action_size=env.action_space.shape[0],
-#                                       hidden_size=200,
-#                                       n_step=n_step,
-#                                       deterministic=True,
-#                                       dynamics_type=dynamics_type,
-#                                       constant_prior=False,
-#                                       prior_scale=0)
-#
-#     policy = lambda x: torch.FloatTensor([[env.action_space.sample()
-#                                            for _ in range(len(x[0]))]
-#                                           for _ in range(len(x))])
-#     dataset = cque.get_sequence_dataset(env_name, dataset_name)
-#     replay_buffer = ReplayBuffer(dataset)
-#
-#     batch = replay_buffer.sample(n=batch_size, chunk_size=horizon)
-#     init_obs = batch.obs[:, 0]
-#     init_action = batch.action[:, 0]
-#     predicted_estimates = mc_return(network, init_obs, init_action, policy,
-#                                     horizon, device='cpu', runs=2,
-#                                     ensemble_mixture=False,
-#                                     step_batch_size=128)
-#
-#     assert predicted_estimates.shape == (batch_size, network.num_ensemble)
-#     assert batch.reward.sum(1) == predicted_estimates.mean(1)
+class FakeNetwork:
+    def __init__(self, env_name):
+        self.env_name = env_name
+        self.env = gym.make(self.env_name)
+        self.num_ensemble = 3
+
+    def reset(self, horizon, batch_size):
+        pass
+
+    def step(self, states, actions):
+        next_states, rewards, dones = [], [], []
+        for batch_i in range(states.shape[0]):
+            e_next_states, e_rewards, e_dones = [], [], []
+            for ensemble in range(self.num_ensemble):
+                state, action = states[batch_i][ensemble], actions[batch_i][ensemble]
+                self.env.reset()
+                self.env.sim.set_state_from_flattened(state.cpu().numpy())
+                self.env.sim.forward()
+                _, reward, done, _ = self.env.step(action.cpu().numpy())
+                next_state = self.env.sim.get_state().flatten()
+                e_next_states.append(next_state.tolist())
+                e_rewards.append(reward)
+                e_dones.append(done)
+
+            next_states.append(e_next_states)
+            rewards.append(e_rewards)
+            dones.append(e_dones)
+        return torch.tensor(next_states), torch.tensor(rewards), torch.tensor(dones)
+
+
+def test_mc_return():
+    import cque
+    from core.utils import mc_return
+    import policybazaar
+
+    env_name = 'Hopper-v2'
+    queries = cque.get_queries(env_name)
+    (policy_a_id, policy_b_id) = list(queries.keys())[0]
+    query_batch = queries[(policy_a_id, policy_b_id)]
+    policy_a, _ = policybazaar.get_policy(*policy_a_id)
+
+    # query-a
+    obss_a = query_batch['info']['state_a']
+    actions_a = query_batch['action_a']
+    horizons = query_batch['horizon']
+    horizon = np.unique(horizons)[0]
+    _filter = horizons == horizon
+    init_obs = obss_a[_filter]
+    init_action = actions_a[_filter]
+    fake_network = FakeNetwork(env_name)
+
+    # we receive state and pass on obs. to actor
+    def _policy(x):
+        # input shape => (batch,ensemble, state)
+        return policy_a.actor(x[:, :, 2:])
+
+    predict = mc_return(fake_network, init_obs, init_action, _policy, horizon,
+                        device='cpu', runs=10, ensemble_mixture=False,
+                        step_batch_size=128)
+    target = query_batch['info']['return_a'][_filter]
+    assert np.mean(predict.mean(1) - target) < 2, \
+        'error between prediction and target is large'
