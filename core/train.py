4f175212b8c2172c34d2754954eac246f5d8d5ea
Anurag Koul <koulanurag@gmail.com>
2022-01-14 02:55:19 -0800

    fix: save model at log interval rate

--- core/train.py
+++ core/train.py
@@ -1,5 +1,4 @@
 import logging
-import random
 
 import cque
 import torch
@@ -49,27 +48,24 @@ def train_dynamics(config: BaseConfig):
     network = network.to(config.device)
 
     # train
-    for update_i in range(0, config.args.update_count,
+    for update_i in range(0, config.args.update_count + 1,
                           config.args.log_interval):
         # estimate ensemble loss and update
         loss: dict = network.update(replay_buffers,
                                     batch_count=config.args.log_interval,
                                     batch_size=config.args.dynamics_batch_size)
         # log
-        if config.args.log_interval:
-            _msg = '#{:<10}'.format(update_i)
-            for k1, v1 in loss.items():
-                for k2, v2 in v1.items():
-                    for k3, v3 in v2.items():
-                        _msg += '{}/{}/{} loss:{:<8.3f}'.format(k1, k2, k3, v3)
-            logging.getLogger('train_dynamics').info(_msg)
-            if config.args.use_wandb:
-                wandb.log({**{'update': update_i}, **loss})
+        _msg = '#{:<10}'.format(update_i)
+        for k1, v1 in loss.items():
+            for k2, v2 in v1.items():
+                for k3, v3 in v2.items():
+                    _msg += '{}/{}/{} loss:{:<8.3f}'.format(k1, k2, k3, v3)
+        logging.getLogger('train_dynamics').info(_msg)
+        if config.args.use_wandb:
+            wandb.log({**{'update': update_i}, **loss})
 
         # save
-        if ((update_i % config.args.dynamics_checkpoint_interval == 0)
-                or update_i == (config.args.update_count - 1)):
-            torch.save({'network': network.state_dict(),
-                        'update': update_i}, config.checkpoint_path)
-            if config.args.use_wandb:
-                wandb.save(glob_str=config.checkpoint_path, policy='now')
+        torch.save({'network': network.state_dict(),
+                    'update': update_i}, config.checkpoint_path)
+        if config.args.use_wandb:
+            wandb.save(glob_str=config.checkpoint_path, policy='now')
