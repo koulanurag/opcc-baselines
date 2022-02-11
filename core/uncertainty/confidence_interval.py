ae27b18adb2996ee9e44b433684100be34929f93
Anurag Koul <koulanurag@gmail.com>
2022-02-10 22:07:24 -0800

    perf: optimized ci estimation

--- core/uncertainty/confidence_interval.py
+++ core/uncertainty/confidence_interval.py
@@ -8,57 +8,50 @@ def paired_confidence_interval(pred_a, pred_b):
     pred_label = np.array([None for _ in range(len(pred_a))])
     pred_conf_level = np.array([None for _ in range(len(pred_a))])
     for conf_level in np.arange(0, 1.01, 0.01):
-        res_low = np.zeros(len(pred_a))
-        res_high = np.zeros(len(pred_a))
-        for idx in range(len(delta_pred)):
-            conf_interval = scipy.stats.t.interval(conf_level,
-                                                   len(delta_pred[idx]) - 1,
-                                                   np.mean(delta_pred[idx]),
-                                                   scipy.stats.sem(
-                                                       delta_pred[idx])
-                                                   + 1e-3)
-            res_low[idx], res_high[idx] = conf_interval
-
+        df = np.ones(len(delta_pred)) * (len(delta_pred[0]) - 1)
+        loc = np.mean(delta_pred, axis=1)
+        scale = scipy.stats.sem(delta_pred, axis=1) + 1e-3
+        conf_interval = scipy.stats.t.interval(conf_level, df, loc, scale)
+        res_low, res_high = conf_interval
         accept_filter = ~np.logical_and((res_low < 0), (res_high > 0))
         pred_label[np.logical_and(res_high < 0, accept_filter)] = 1  # true
         pred_label[np.logical_and((res_low > 0), accept_filter)] = 0  # false
         pred_conf_level[accept_filter] = conf_level
 
+    pred_label = pred_label.astype(bool)
     return pred_label, pred_conf_level
 
 
 def unpaired_confidence_interval(pred_a, pred_b):
-    pred_label = [None for _ in range(len(pred_a))]
-    pred_conf = [None for _ in range(len(pred_a))]
+    pred_label = np.array([None for _ in range(len(pred_a))])
+    pred_conf = np.array([None for _ in range(len(pred_a))])
     for conf_level in np.arange(0, 1.01, 0.01):
-        res_low_a = np.zeros(len(pred_a))
-        res_high_a = np.zeros(len(pred_a))
-        res_low_b = np.zeros(len(pred_b))
-        res_high_b = np.zeros(len(pred_b))
-        for idx in range(len(pred_a)):
-            conf_interval = scipy.stats.t.interval(conf_level,
-                                                   len(pred_a[idx]) - 1,
-                                                   np.mean(pred_a[idx]),
-                                                   scipy.stats.sem(pred_a[idx])
-                                                   + 1e-3)
-            res_low_a[idx], res_high_a[idx] = conf_interval
+        df = np.ones(len(pred_a)) * (len(pred_a[0]) - 1)
+        loc = np.mean(pred_a, axis=1)
+        scale = scipy.stats.sem(pred_a, axis=1) + 1e-3
+        conf_interval = scipy.stats.t.interval(conf_level, df, loc, scale)
+        res_low_a, res_high_a = conf_interval
 
-            conf_interval = scipy.stats.t.interval(conf_level,
-                                                   len(pred_b[idx]) - 1,
-                                                   np.mean(pred_b[idx]),
-                                                   scipy.stats.sem(pred_b[idx])
-                                                   + 1e-3)
-            res_low_b[idx], res_high_b[idx] = conf_interval
+        df = np.ones(len(pred_b)) * (len(pred_b[0]) - 1)
+        loc = np.mean(pred_b, axis=1)
+        scale = scipy.stats.sem(pred_b, axis=1) + 1e-3
+        conf_interval = scipy.stats.t.interval(conf_level, df, loc, scale)
+        res_low_b, res_high_b = conf_interval
 
-        abstain_filter = np.logical_or((res_low_a < res_low_b < res_high_a),
-                                       (res_low_a < res_high_b < res_high_a))
+        abstain_filter = np.logical_or(np.logical_and(res_low_a <= res_low_b,
+                                                      res_low_b <= res_high_a),
+                                       np.logical_and(res_low_a <= res_high_b,
+                                                      res_high_b <= res_high_a)
+                                       )
         accept_filter = ~abstain_filter
+
         # true
         pred_label[np.logical_and(res_high_a < res_low_b, accept_filter)] = 1
         # false
         pred_label[np.logical_and((res_low_a > res_high_b), accept_filter)] = 0
         pred_conf[accept_filter] = conf_level
 
+    pred_label = pred_label.astype(bool)
     return pred_label, pred_conf
 
 
