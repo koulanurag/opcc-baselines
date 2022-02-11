f3ae4cd9d685f98632cb9ae4fe9cc3dd3de2ec69
Anurag Koul <koulanurag@gmail.com>
2022-02-10 16:12:33 -0800

    fix: prettified latex table

--- scripts/summary_plot.py
+++ scripts/summary_plot.py
@@ -6,6 +6,20 @@ import pandas as pd
 import wandb
 
 
+def camel_case_split(str):
+    start_idx = [i for i, e in enumerate(str)
+                 if e.isupper()] + [len(str)]
+
+    start_idx = [0] + start_idx
+    return [str[x: y] for x, y in zip(start_idx, start_idx[1:])]
+
+
+def score(metric_info):
+    return "$" + (str(round(metric_info['mean'], 3))
+                  + "\pm"
+                  + str(round(metric_info['std'], 2))) + "$"
+
+
 def metrics(data, key):
     info = {}
     for _num in np.unique(data[key].values):
@@ -29,45 +43,49 @@ def metrics(data, key):
     return info
 
 
+def prettify_env_name(env_name):
+    if "d4rl:maze2d-" in env_name:
+        x = env_name.split("d4rl:maze2d-")[1].split("-v")[0]
+        return x
+    else:
+        x = env_name.split("-v")[0]
+        return "\\\\".join(camel_case_split(x)[1:])
+
+
 def latex_table(info_dict, category_name, table_name, path):
     tex = "\\begin{table}[t!]" + "\n" + \
-          "\caption{Evaluation metrics for  \emph{dataset quality}" + \
+          "\caption{Evaluation metrics for  \emph{" + table_name + "}" + \
           " comparison in \emph{" + category_name + "} environments } " + "\n" + \
-          "\\vspace{-1em}" + "\n" + \
           "\label{table:" + category_name + '-' + table_name + "}" + "\n"
     tex += "\\begin{center} " + "\n" + \
            "\\begin{footnotesize} " + "\n" + \
            "\\begin{sc} " + "\n" + \
            "\\begin{tabular}{|l | c | c | c| c |}" + "\n" + \
-           "\\toprule"
+           "\\toprule" + "\n"
     tex += "Env. & " + table_name + " & AURCC$(\\downarrow)$ &" \
                                     " RPP$(\\downarrow)$ &" \
                                     " $CR_K(\\uparrow)$ \\\\" + '\n'
     tex += "\\midrule" + '\n'
     for env_i, env_name in enumerate(info_dict.keys()):
-        tex += "\multirow{5}{3.6em}{" + env_name + "}  "
+        tex += "\multirow{" + str(len(info_dict[env_name])) \
+               + "}{3.6em}{" + str(prettify_env_name(env_name)) + "}  "
         for cat in info_dict[env_name]:
             tex += "& " \
                    + str(cat) \
-                   + " & " + str(round(info_dict[env_name]
-                                       [cat]['aurcc']['mean'], 3)) \
-                   + " & " + str(round(info_dict[env_name]
-                                       [cat]['rpp']['mean'], 3)) \
-                   + " & " + str(round(info_dict[env_name]
-                                       [cat]['cr_10']['mean'], 3)) \
+                   + " & " + score(info_dict[env_name][cat]['aurcc']) \
+                   + " & " + score(info_dict[env_name][cat]['rpp']) \
+                   + " & " + score(info_dict[env_name][cat]['cr_10']) \
                    + " \\\\ \n"
         if env_i == len(info_dict) - 1:
             tex += "\\bottomrule"
         else:
             tex += "\\midrule"
-    tex += """
-    \\end{tabular}
-    \\end{sc} 
-    \\end{footnotesize} 
-    \\end{center} 
-    \\vspace{-2.2em}
-    \\end{table}
-    """
+    tex += "\\end{tabular}" + "\n"
+    tex += "\\end{sc}" + "\n"
+    tex += "\\end{footnotesize} " + "\n"
+    tex += "\\end{center} " + "\n"
+    tex += "\\end{table}" + "\n"
+
     os.makedirs(path, exist_ok=True)
     with open(os.path.join(path, 'metrics.tex'), 'w') as f:
         f.write(tex)
@@ -99,7 +117,7 @@ def generate_table_tex(category_name, data_df):
                         & (np.isnan(data_df['horizon'].values))]
     # ensemble-count
     ensemble_count_info = metrics(base_data, 'ensemble_count')
-    latex_table(ensemble_count_info, category_name, 'ensemble_count',
+    latex_table(ensemble_count_info, category_name, 'ensemble-count',
                 os.path.join(root_dir, 'ensemble-count'))
     dataset_quality_info = metrics(base_data, 'dataset_name')
     latex_table(dataset_quality_info, category_name, 'dataset-quality',
@@ -123,7 +141,6 @@ def generate_table_tex(category_name, data_df):
     # ##############################
     # constant-prior
     # ##############################
-    constant_prior_info = {}
     prior_data = data_df[(data_df['deterministic'] == base_deter)
                          & (data_df['mixture'] == base_mixture)
                          & (data_df['dynamics_type'] == base_dyn_type)
@@ -134,7 +151,7 @@ def generate_table_tex(category_name, data_df):
                             base_uncertainty_type)
                          & np.isnan(data_df['horizon'].values)]
     prior_info = metrics(prior_data, 'constant_prior_scale')
-    latex_table(prior_info, category_name, 'prior_scale',
+    latex_table(prior_info, category_name, 'prior-scale',
                 os.path.join(root_dir, 'prior-scale'))
 
     # ##############################
@@ -157,7 +174,6 @@ def generate_table_tex(category_name, data_df):
     # ##############################
     # dynamics-type
     # ##############################
-    dynamics_type_info = {}
     dyn_data = data_df[(data_df['constant_prior_scale'] == base_prior_scale)
                        & (data_df['deterministic'] == base_deter)
                        & (data_df['mixture'] == base_mixture)
@@ -168,7 +184,7 @@ def generate_table_tex(category_name, data_df):
                           base_uncertainty_type)
                        & np.isnan(data_df['horizon'])]
     dynamics_type_info = metrics(dyn_data, 'dynamics_type')
-    latex_table(deterministic_info, category_name, 'dynamics-type',
+    latex_table(dynamics_type_info, category_name, 'dynamics-type',
                 os.path.join(root_dir, 'dynamics-type'))
 
     # ##############################
@@ -182,10 +198,25 @@ def generate_table_tex(category_name, data_df):
                        & (data_df['clip_reward'] == base_clip_reward)
                        & (data_df['uncertainty_type'] ==
                           base_uncertainty_type)
-                       & (data_df['horizon'] == np.nan)]
+                       & np.isnan(data_df['horizon'])]
     mixture_info = metrics(mix_data, 'mixture')
     latex_table(mixture_info, category_name, 'mixture',
                 os.path.join(root_dir, 'mixture'))
+
+    # ##############################
+    # uncertainty-test type
+    # ##############################
+    type_data = data_df[(data_df['constant_prior_scale'] == base_prior_scale)
+                        & (data_df['deterministic'] == base_deter)
+                        & (data_df['mixture'] == base_mixture)
+                        & (data_df['dynamics_type'] == base_dyn_type)
+                        & (data_df['normalize'] == base_normalize)
+                        & (data_df['clip_obs'] == base_clip_obs)
+                        & (data_df['clip_reward'] == base_clip_reward)
+                        & (np.isnan(data_df['horizon'].values))]
+    mixture_info = metrics(type_data, 'uncertainty_type')
+    latex_table(mixture_info, category_name, 'uncertainty-type',
+                os.path.join(root_dir, 'uncertainty-type'))
 
 
 def main():
